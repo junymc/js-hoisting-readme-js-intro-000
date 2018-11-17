21ba4e248af2af46802c0ddfb85b910828ec386d
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-17 03:30:42 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -9,23 +9,23 @@ function callMe() {
 }
 
 
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-}
-
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
-}
+// function crazy() {
+//   // fix the code in here:
+//   thisIsCrazy();
+// 
+//   var thisIsCrazy = function (){
+//     console.log("hey!!!")
+//   }
+// }
+// 
+// function sayMyName() {
+//   // fix the code in here:
+//   var name = "Cricky";
+// 
+//   sayMy();
+// 
+//   function sayMy() {
+//     console.log(name);
+//     var name = "Kristin";
+//   }
+// }
